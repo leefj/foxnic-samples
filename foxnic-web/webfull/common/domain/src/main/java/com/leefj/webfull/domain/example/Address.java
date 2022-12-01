b5d638c39b1f2bb88e0c412a8a754fc0a640a039
lifangjie <leefangjie@qq.com>
2022-12-01 10:55:12 +0800

    提交

--- foxnic-web/webfull/common/domain/src/main/java/com/leefj/webfull/domain/example/Address.java
+++ foxnic-web/webfull/common/domain/src/main/java/com/leefj/webfull/domain/example/Address.java
@@ -23,7 +23,7 @@ import com.github.foxnic.sql.data.ExprRcd;
  * 订单地址
  * <p>订单地址 , 数据表 webfull_example_address 的PO类型</p>
  * @author 李方捷 , leefangjie@qq.com
- * @since 2022-11-10 10:39:38
+ * @since 2022-12-01 09:09:20
  * @sign ECBAC43BBB55CF099B8D084BD30689BD
  * 此文件由工具自动生成，请勿修改。若表结构或配置发生变动，请使用工具重新生成。
 */
